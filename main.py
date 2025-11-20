86a8daa7dcc236cf07f764ced62274ab63dbc4cf
otitoko <tbkopera@proton.me>
2025-11-20 14:01:09 +0100

    buttons for accepting and rejecting

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+import sys
 
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMainWindow, QGridLayout, QWidget
 
-app=QApplication(sys.argv)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        layout=QGridLayout()
+
+        yes_button = QPushButton("Accept Email")
+        layout.addWidget(yes_button,3,2)
+
+        no_button = QPushButton("Reject Email")
+        layout.addWidget(no_button,3,1)
+
+        yes_button.setCheckable(True)
+        no_button.setCheckable(True)
+        yes_button.clicked.connect(self.accept_email)
+        no_button.clicked.connect(self.reject_email)
+
+        widget=QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def accept_email(self):
+        print("Email is ok to send off")
+
+    def reject_email(self):
+        print("Email needs to be reviewed")
+        
+    def print_contents(self):
+        return
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
