5c2403b0bc8d7d2c83fba2a3c860861030ffebaa
otitoko <tbkopera@proton.me>
2025-11-20 15:19:50 +0100

    env vars get accounted for and added loop for the file list

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import sys
+import os
+import shutil
 
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMainWindow, QGridLayout, QWidget
+
+accepted_dir=os.environ.get('ACCEPTED_EMAILS')
+rejected_dir=os.environ.get('REJECTED_EMAILS')
+review_pending_dir=os.environ.get('REVIEW_PENDING')
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,14 +32,25 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
+        file_list = [file for file in os.listdir(f"{REVIEW_PENDING}")]
+
+        email_text=read_contents(file)
+        email_body=QLabel(email_text)
+        layout.addWidget(email_body,2,1)
+
+
     def accept_email(self):
         print("Email is ok to send off")
+        shutil.copyfile(review_pending_dir/file,accepted_dir/file)
 
     def reject_email(self):
         print("Email needs to be reviewed")
-        
-    def print_contents(self):
-        return
+        shutil.copyfile(review_pending_dir/file,rejected_dir/file)
+
+    def read_contents(file):
+        file_path=open(f"{review_pending}/{file}")
+        content=file_path.read()
+        return content
 
 app = QApplication(sys.argv)
 
