b6d9656b6d41137aaa58c381c67c8476e00d6652
otitoko <tbkopera@proton.me>
2025-11-27 14:21:47 +0100

    implemented JSON export function, viewing capabilities and exit button

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import shutil
+from pathlib import Path
 
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMainWindow, QGridLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMainWindow, QGridLayout, QWidget, QLabel
 
 accepted_dir=os.environ.get('ACCEPTED_EMAILS')
 rejected_dir=os.environ.get('REJECTED_EMAILS')
@@ -18,39 +19,54 @@ class MainWindow(QMainWindow):
         layout=QGridLayout()
 
         yes_button = QPushButton("Accept Email")
-        layout.addWidget(yes_button,3,2)
+        layout.addWidget(yes_button,3,0)
 
         no_button = QPushButton("Reject Email")
         layout.addWidget(no_button,3,1)
 
+        exit_button = QPushButton("Exit")
+        layout.addWidget(exit_button, 3, 3)
+
         yes_button.setCheckable(True)
         no_button.setCheckable(True)
+        exit_button.setCheckable(True)
         yes_button.clicked.connect(self.accept_email)
         no_button.clicked.connect(self.reject_email)
+        exit_button.clicked.connect(self.exit)
 
         widget=QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
-        file_list = [file for file in os.listdir(f"{REVIEW_PENDING}")]
+        file_list = [file for file in os.listdir(review_pending_dir)]
 
-        email_text=read_contents(file)
-        email_body=QLabel(email_text)
-        layout.addWidget(email_body,2,1)
+        for file in file_list:
+            email_text=self.read_contents(file)
+            email_body=QLabel(email_text)
+            email_body.setWordWrap(True)
+            layout.addWidget(email_body,2,0)
 
 
-    def accept_email(self):
+    def accept_email(self,file):
         print("Email is ok to send off")
-        shutil.copyfile(review_pending_dir/file,accepted_dir/file)
+        review_pending_filepath=Path(review_pending_dir)/file
+        accepted_filepath=Path(accepted_dir)/file
+        shutil.copyfile(review_pending_filepath,accepted_filepath)
 
-    def reject_email(self):
+    def reject_email(self,file):
         print("Email needs to be reviewed")
-        shutil.copyfile(review_pending_dir/file,rejected_dir/file)
+        review_pending_filepath=Path(review_pending_dir)/file
+        rejected_filepath=Path(rejected_dir)/file
+        shutil.copyfile(review_pending_filepath,rejected_filepath)
 
-    def read_contents(file):
-        file_path=open(f"{review_pending}/{file}")
+    def read_contents(self,file):
+        review_pending_filepath=Path(review_pending_dir)/file
+        file_path=open(review_pending_filepath)
         content=file_path.read()
         return content
+
+    def exit(self):
+        exit()
 
 app = QApplication(sys.argv)
 
